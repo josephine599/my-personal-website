41838a1ccf0fdb8de0b82287316d7d098d07cb61
josephine anyango <anyangojozey@gmail.com>
2025-08-29 14:59:08 +0300

    Add my-personal-website content

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const h2 = document.createElement(h2);
-h2.textcontent = "This content added by Javascript";
 
-document.querySelector("body").appendChild(h2);
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Welcome to my portfolio!");
+});
+
